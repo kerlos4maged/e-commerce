db5a382f3c3e9852aa1f133ec78951a9007b3309
kerlosmaged <kerlosmaged55@gmail.com>
2024-09-20 18:39:57 +0300

    last updated

--- controllers/order_controller.js
+++ controllers/order_controller.js
@@ -16,6 +16,24 @@ const productModel = require('../models/product_models')
 const { sendEmail } = require('../utils/usingEmailToSend')
 
 const cartModel = require('../models/cart_models')
+const userModel = require('../models/user_models')
+
+const cangeProductQunatityAfterCreateOrder = async (order, cart, cartId) => {
+    // 4- after make the order complete check the quantity for the product and how much is sold
+    if (order) {
+        const bulkOption = cart.products.map((productItem) => ({
+            updateOne: {
+                filter: { _id: productItem.product },
+                update: {
+                    $inc: { quantity: -productItem.quantity, sold: +productItem.quantity },
+                },
+            },
+        }))
+        await productModel.bulkWrite(bulkOption, {})
+        // 5- clear the user cart after make the order complete depending on id
+        await cartModel.findByIdAndDelete(cartId)
+    }
+}
 
 // payment on cash orders 
 // @route -> Post Api/v1/orders/:cartItemId
@@ -44,29 +62,16 @@ const payCashOrderController = asyncHandler(async (req, res, next) => {
         shippingAddress: req.body.shippingAddress,
     })
     console.log(order)
+
     // 4- after make the order complete check the quantity for the product and how much is sold 
-    if (order) {
-        const bulkOption = cart.products.map((productItem) => ({
-            updateOne: {
-                filter: { _id: productItem.product },
-                update: {
-                    $inc: { quantity: -productItem.quantity, sold: +productItem.quantity },
-                },
-            },
-        }))
-        await productModel.bulkWrite(bulkOption, {})
-        // 5- clear the user cart after make the order complete depending on id
-        await cartModel.findByIdAndDelete(req.params.id)
+    cangeProductQunatityAfterCreateOrder(order, cart, req.params.id)
 
-        // 6- send email to the user with order details
-        await sendEmail({
-            to: req.user.email,
-            subject: 'Order placed successfully',
-            message: `Your order has been placed successfully. Order ID: ${order._id}\nyour total price is:${order.totalOrderPrice}`,
-        })
-
-    }
-
+    // 6- send email to the user with order details
+    await sendEmail({
+        to: req.user.email,
+        subject: 'Order placed successfully',
+        message: `Your order has been placed successfully. Order ID: ${order._id}\nyour total price is:${order.totalOrderPrice}`,
+    })
     res.status(201).json({
         success: true,
         message: 'Order placed successfully checked your email ',
@@ -177,6 +182,29 @@ const createSessionUsingString = asyncHandler(
         })
     })
 
+const createCartOrders = async (sessions) => {
+    // 1- get cart id
+    const cartId = sessions.client_reference_id
+    // 2- amount price 
+    const amount = sessions.amount_total / 100 // because when creating a new session multiple amount on 100 
+
+    const cart = await cartModel.findById(cartId)
+    const user = userModel.findOne({ email: sessions.email })
+    // 3- create order and save it in database
+
+    const order = await orderModel.create({
+        user: user._id,
+        cart: cart.products,
+        totalOrderPrice: amount,
+        shippingAddress: sessions.metadata,
+        paymentMethod: 'card',
+        isPaid: true,
+        paidAt: Date.now(),
+    })
+    console.log(order)
+    cangeProductQunatityAfterCreateOrder(order, cart, cartId)
+}
+
 const createOrderOnlineUsingStripe = (req, res) => {
     const sig = req.headers['stripe-signature'];
 
@@ -204,15 +232,18 @@ const createOrderOnlineUsingStripe = (req, res) => {
             console.log(`Unhandled event type ${event.type}`);
     }
 
-    console.log(`this is value returned from sessions: ${session}`);
+    console.log(`this is value returned from sessions: ${JSON.stringify(session)}`);
+
+    // all the code before this comment get it from stripe webhook creation
 
     if (event.type === 'checkout.session.completed') {
         console.log(`session data after completed: ${event.data.object.client_reference_id}`);
         // create order here 
-        res.status(201).send({
-            Status: "Success",
-            message: "payment received"
-        });
+        createCartOrders(event.data.object)
+        res.status(200).send({
+            "Status": "Success",
+            "message": "received order successfully",
+        })
     } else {
         res.status(400).send({
             Status: "Failed",
@@ -220,7 +251,6 @@ const createOrderOnlineUsingStripe = (req, res) => {
             Error: "checkout session expired"
         });
     }
-
 
 }
 
