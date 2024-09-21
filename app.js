2f63bc60ed1d6faa65aa4e0fc468981e4390ccd0
kerlosmaged <kerlosmaged55@gmail.com>
2024-09-21 15:46:33 +0300

    last update final version

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ app.post(
 
 const limit = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    limit: 1000, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+    limit: 10, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
     standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
     message: "Too many requests"
